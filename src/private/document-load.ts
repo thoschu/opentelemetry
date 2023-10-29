96acfcd2e2072e74651c1d16aa137f26116cbe3d
Tom S <thoschulte@gmail.com>
2023-10-29 22:25:20 +0100

    update

--- src/private/document-load.ts
+++ src/private/document-load.ts
@@ -1,9 +1,16 @@
+import { ConsoleSpanExporter, SimpleSpanProcessor } from '@opentelemetry/sdk-trace-base';
 import { WebTracerProvider } from '@opentelemetry/sdk-trace-web';
 import { DocumentLoadInstrumentation } from '@opentelemetry/instrumentation-document-load';
 import { ZoneContextManager } from '@opentelemetry/context-zone';
 import { registerInstrumentations } from '@opentelemetry/instrumentation';
+import { UserInteractionInstrumentation } from '@opentelemetry/instrumentation-user-interaction';
+import { XMLHttpRequestInstrumentation } from '@opentelemetry/instrumentation-xml-http-request';
+import { Resource } from '@opentelemetry/resources';
 
-const provider: WebTracerProvider = new WebTracerProvider();
+const hostname: string = 'front-end';
+const provider: WebTracerProvider = new WebTracerProvider({ resource: new Resource({'x.host.name': hostname})});
+
+provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
 
 provider.register({
     // Changing default contextManager to use ZoneContextManager - supports asynchronous operations - optional
@@ -12,7 +19,10 @@ provider.register({
 
 // Registering instrumentations
 registerInstrumentations({
-    instrumentations: [new DocumentLoadInstrumentation()],
+    instrumentations: [
+        new DocumentLoadInstrumentation(),
+        new UserInteractionInstrumentation(),
+        new XMLHttpRequestInstrumentation()
+    ],
+    tracerProvider: provider
 });
-
-console.log(provider);
