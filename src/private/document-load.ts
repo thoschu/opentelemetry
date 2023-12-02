efb4b6cb5ae3d130fa689e08e9fc0047a1ebb0f9
Tom S <thoschulte@gmail.com>
2023-12-02 21:08:33 +0100

    update

--- src/private/document-load.ts
+++ src/private/document-load.ts
@@ -1,15 +1,25 @@
 import { ConsoleSpanExporter, SimpleSpanProcessor } from '@opentelemetry/sdk-trace-base';
-import { WebTracerProvider } from '@opentelemetry/sdk-trace-web';
+import { BatchSpanProcessor, WebTracerProvider } from '@opentelemetry/sdk-trace-web';
 import { DocumentLoadInstrumentation } from '@opentelemetry/instrumentation-document-load';
 import { ZoneContextManager } from '@opentelemetry/context-zone';
 import { registerInstrumentations } from '@opentelemetry/instrumentation';
 import { UserInteractionInstrumentation } from '@opentelemetry/instrumentation-user-interaction';
 import { XMLHttpRequestInstrumentation } from '@opentelemetry/instrumentation-xml-http-request';
 import { Resource } from '@opentelemetry/resources';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
 
 const hostname: string = 'front-end';
-const provider: WebTracerProvider = new WebTracerProvider({ resource: new Resource({'x.host.name': hostname})});
+const provider: WebTracerProvider = new WebTracerProvider({
+    resource: new Resource({
+        'x.host.name': hostname
+    })
+});
 
+const traceExporter: OTLPTraceExporter = new OTLPTraceExporter({
+    url: 'http://localhost:4318/v1/traces'
+});
+
+provider.addSpanProcessor(new BatchSpanProcessor(traceExporter));
 provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
 
 provider.register({
